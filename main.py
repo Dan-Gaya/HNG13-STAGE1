195430f1e6976b4def6cf6926ec80ed7a0d5eee2
Dan-Gaya <kaniabdulaziznura@gmail.com>
2025-10-27 11:38:37 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,41 @@
+from fastapi import FastAPI
+from controller.controllers import router
+from fastapi.middleware.cors import CORSMiddleware
+import logging
+import uvicorn
+from contextlib import asynccontextmanager
+
+
+app = FastAPI(title="String Analyzer Service (In-Memory)", version="1.0.0")
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+# Add CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Restrict in production
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+# Include routers
+app.include_router(router)
+
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome to the String Analyzer Service API"}
+
+
+# if __name__ == "__main__":
+#     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+
+if __name__ == "__main__":
+    import os
+    port = int(os.getenv("PORT", 8000))  # PXXL sets this automatically
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
